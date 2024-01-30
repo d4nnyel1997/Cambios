cefac1f27dc63e58264c32c634ae7764203799f6
d4nnyel1997 <d4nnyel1997@gmail.com>
2024-01-30 10:53:22 -0500

    get push origin Cambios

---
+++ main.js
@@ -0,0 +1,7 @@
+var nombre = "daniel muñoz";
+var edad = 20;
+var altura = 180;
+
+var concatenacion = "mi nombre es " + nombre + " y mido " + altura + "CM";
+var datos = document.getElementById("datos")
+datos.innerHTML = concatenacion;
